9587d8de5c9ee71ae1297169d5d84d1590f3ba20
Alewiya <duressaalewiya@gmail.com>
2019-11-22 12:40:48 -0600

    final-project

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask,request,render_template,redirect
 app = Flask(__name__)
-app.config['DEBUG'] = True    
+app.config['DEBUG'] = True 
+
 @app.route("/")
 def index():
     return render_template('index.html')
+
 @app.route("/index", methods=['POST'])
 def signup():
     username= request.form['username']
@@ -14,30 +16,34 @@ def signup():
     password_error=''
     verify_error=''
     email_error=''
-    if (username.isspace()==True) and (len(username) < 0):
+    if int(len(username)) <= 0 :
         username_error="That's not a valid username"
         username=''
-    else:
-        if (password.isspace()==True) and int(len(password) < 3) and int(len(password)>20) and int(len(password)<0):
+    elif int(len(username)) < 3 or int(len(username)) > 20 or " " in username :
+            username_error = 'Thats not a valid username'
+            username = ''  
+    if int(len(password)) <= 0:
+            password_error="That's not a valid password"
+            password=''
+    elif int(len(password)) < 3 or int(len(password)) > 20:
             password_error="That's not a valid password"
             password=''
     if verify != password:
-        verify_error="That's do  not match"
+        verify_error="passowrd do not match"
         verify=''
-    if int(len(email))>0:
-        if "@" not in email and '.' not in email and " " not in email:
+    if int(len(email)) > 0:
+        if "@" != email and '.' != email and " " != email:
             email_error="That's not a valid email"
             email=''
-        else:
-            if int(len(email) <3 or int(len(email)>20)):
+        elif int(len(email)) < 3 or int(len(email)) > 20:
                 email_error="That's not a valid email"
                 email=''
     if not username_error and not password_error and not verify_error and not email_error:
-        return render_template('homepage.html',username=username)
-    
-    return render_template('index.html',username_error=username_error,password_error=password_error,verify_error=verify_error,email_error=email_error,username=username,password=password,verify=verify,email=email)
+        return redirect('/homepageusername={0}'.format(username))
+    else:
+        return render_template('index.html',username_error=username_error,password_error=password_error,verify_error=verify_error,email_error=email_error,username=username,password=password,verify=verify,email=email)
 @app.route('/homepage')
 def greeting():
-    username=request.args.get('username')
+    username=request.form['username']
     return render_template('homepage.html',username=username)
 app.run()
